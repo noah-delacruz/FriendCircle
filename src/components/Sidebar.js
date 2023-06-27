e691dbd676943722222aaef09ed36bb9c99f1104
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-06-27 02:42:42 -0700

    Layout the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,83 @@
-import { Box } from '@mui/material'
+import { AccountBox, Article, DarkMode, Group, Home, Person, Settings, Storefront } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 export default function Sidebar() {
     return (
-        <Box bgcolor="red" flex={1} sx={{ display: { xs: "none", sm: "block"}}}>
-            Sidebar
+        <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block"}}}>
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                    <ListItemIcon>
+                        <Home/>
+                    </ListItemIcon>
+                    <ListItemText primary="Homepage" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#pages">
+                    <ListItemIcon>
+                        <Article/>
+                    </ListItemIcon>
+                    <ListItemText primary="Pages" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#groups">
+                    <ListItemIcon>
+                        <Group/>
+                    </ListItemIcon>
+                    <ListItemText primary="Groups" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#marketplace">
+                    <ListItemIcon>
+                        <Storefront/>
+                    </ListItemIcon>
+                    <ListItemText primary="Marketplace" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#friends">
+                    <ListItemIcon>
+                        <Person/>
+                    </ListItemIcon>
+                    <ListItemText primary="Friends" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#settings">
+                    <ListItemIcon>
+                        <Settings/>
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#profile">
+                    <ListItemIcon>
+                        <AccountBox/>
+                    </ListItemIcon>
+                    <ListItemText primary="Profile" />
+                    </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                    <ListItemButton component="a" href="#home">
+                    <ListItemIcon>
+                        <DarkMode/>
+                    </ListItemIcon>
+                    <Switch/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
         </Box>
     )
 }
