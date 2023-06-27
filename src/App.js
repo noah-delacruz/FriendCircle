f1464611eef1665b8df50330f0cce7b08a331bc1
Noah dela Cruz <delacruznoah12898@yahoo.com>
2023-06-27 02:13:57 -0700

    Finish the Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
-import Rightbar from './components/Rightbar';
-import Feed from './components/Feed';
-import { Box, Stack } from '@mui/material';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import { Box, Stack } from '@mui/material';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,79 @@
-import { AppBar, Toolbar } from '@mui/material'
+import { Mail, Notifications, Pets } from '@mui/icons-material'
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, Toolbar, Typography, styled } from '@mui/material'
 import React from 'react'
 
+const StyledToolbar = styled(Toolbar)({
+    display:"flex",
+    justifyContent:"space-between"
+})
+
+const Search = styled("div")(({theme}) => ({
+    backgroundColor: "white",
+    padding:"0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%"
+}))
+
+const Icons = styled(Box)(({theme}) => ({
+    display:"none",
+    gap:"20px",
+    alignItems:"center",
+    [theme.breakpoints.up("sm")]:{
+        display:"flex"
+    }
+}))
+
+const UserBox = styled(Box)(({theme}) => ({
+    display:"flex",
+    gap:"10px",
+    alignItems:"center",
+    [theme.breakpoints.up("sm")]:{
+        display:"none"
+    }
+}))
+
 export default function Navbar() {
+    const [open, setOpen] = React.useState(false)
     return (
-        <AppBar position="stick">
-            <Toolbar>
-                Navbar
-            </Toolbar>
+        <AppBar position="sticky">
+            <StyledToolbar>
+                <Typography variant='h6' sx={{display:{xs:"none", sm:"block"}}}>Navbar</Typography>
+                <Pets sx={{display:{xs:"block", sm:"none"}}}/>
+                <Search><InputBase placeholder='search...'/></Search>
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail />
+                    </Badge>
+                    <Badge badgeContent={4} color="error">
+                        <Notifications />
+                    </Badge>
+                    <Avatar sx={{width:30, height:30}} src="https://images.pexels.com/photos/5384445/pexels-photo-5384445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                        onClick={e => setOpen(true)}
+                    />
+                </Icons>
+                <UserBox onClick={e => setOpen(true)}>
+                    <Avatar sx={{width:30, height:30}} src="https://images.pexels.com/photos/5384445/pexels-photo-5384445.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                    <Typography variant='span'>John</Typography>
+                </UserBox>
+            </StyledToolbar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={e => setOpen(false)}
+                anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+                }}
+            >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+                <MenuItem>Logout</MenuItem>
+            </Menu>
         </AppBar>
     )
 }
